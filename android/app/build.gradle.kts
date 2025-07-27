8d259488798b7f17dd14f6d63b20914009e156cf
🇨🇳钟智强 『江西青垣科技』 <johnmelodymel@qq.com>
2025-07-27 18:44:46 +0800

    build(android): 更新版本号并添加签名配置
    更新应用版本号至1.0.2+2
    添加Android签名配置，从key.properties加载密钥信息

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,13 +1,30 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+// ✅ 加载签名配置
+val keystoreProperties = Properties().apply {
+    load(rootProject.file("key.properties").inputStream())
+}
+
 android {
+
     namespace = "xin.ctkqiang.identity_scanner"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" 
+
+
+    defaultConfig {
+        applicationId = "xin.ctkqiang.identity_scanner"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -15,26 +32,15 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
-    defaultConfig {
-
-        applicationId = "xin.ctkqiang.identity_scanner"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        // flutter build apk --release \
-        //   --obfuscate \
-        //   --split-debug-info=build/debug-info
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        jvmTarget = "11"
     }
 
     signingConfigs {
-        create("release") { 
-            storeFile = file("upload-keystore.jks")
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
         }
     }
 
@@ -42,7 +48,7 @@ android {
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
-            signingConfig = null
+            signingConfig = signingConfigs.getByName("release")
 
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
